9dba0a510c8087da4d13c8e621b3c7f86f690154
Akauã Carvalho <akaaua@gmail.com>
2020-04-28 19:31:12 -0300

    Changed the way username and password is entered and the repository is selected

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 from selenium import webdriver
 from time import sleep
-from util.secrets import pw
-from util.secrets import username
+from util.secrets import defineusername
+from util.secrets import definepassword
+from util.secrets import definerepository
+#from util.secrets import username
 from datetime import datetime
 import os
 
+#username = definevariables()[0]
+#pw = definevariables()[1]
+#repo = definevariables()[2]
+
 class GitBot:
 
-    def __init__(self, username, pw):        
-        self.username = username
+    def __init__(self):
+           
+        #self.username = username
         self.driver = webdriver.Chrome(os.path.abspath(os.getcwd())+"/util/chromedriver")
         self.driver.get("https://github.com/")
         self.driver.maximize_window()
         self.driver.implicitly_wait(1)
         self.driver.find_element_by_xpath('/html/body/div[1]/header/div/div[2]/div[2]/a[1]').click()
         self.driver.implicitly_wait(1)
-        if username == '':
-            username = input(' ' + ' Qual o nome de usuário do GitHub?')
-        self.driver.find_element_by_xpath('//*[@id="login_field"]').send_keys(username)
-        if pw == '':
-            pw = input(' ' + ' Qual a senha do GitHub?')
-        self.driver.find_element_by_xpath('//*[@id="password"]').send_keys(pw)
+        #if username == '':
+            #username = input(' ' + ' Qual o nome de usuário do GitHub?')
+        self.driver.find_element_by_xpath('//*[@id="login_field"]').send_keys(defineusername())
+        #if pw == '':
+            #pw = input(' ' + ' Qual a senha do GitHub?')
+        self.driver.find_element_by_xpath('//*[@id="password"]').send_keys(definepassword())
         self.driver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]').click()
         sleep(1)
 
 
     def _select_project(self):
-        repo = '' #for test purposes define here.
-        if repo == '':
-            repo = input(' ' + ' Qual o nome do repositório?')
-        self.driver.find_element_by_xpath('//*[@title="' + repo + '"]').click()     
+        #repo = ''#for test purposes define here.
+        #if repo == '':
+            #repo = input(' ' + ' Qual o nome do repositório?')
+        self.driver.find_element_by_xpath('//*[@title="' + definerepository() + '"]').click()     
 
     def _get_datetime(self):
         
@@ -47,9 +54,8 @@ class GitBot:
 
 #sleep(120)
 
-
-my_bot = GitBot(username, pw)
-
+#my_bot = Secrets()
+my_bot = GitBot()
 my_bot._select_project()
 my_bot._get_datetime()
 
