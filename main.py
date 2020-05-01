291e1ce59581a4ada169f538f86b119e55885223
Akauã Carvalho <akaaua@gmail.com>
2020-05-01 15:53:27 -0300

    Formatted the date to Day, month, year format.

--- main.py
+++ main.py
@@ -3,59 +3,50 @@ from time import sleep
 from util.definevariables import defineusername
 from util.definevariables import definepassword
 from util.definevariables import definerepository
-#from util.secrets import username
-from datetime import datetime
+import datetime as dt
 import os
 
-#username = definevariables()[0]
-#pw = definevariables()[1]
-#repo = definevariables()[2]
 
 class GitBot:
 
     def __init__(self):
-           
-        #self.username = username
+                
         self.driver = webdriver.Chrome(os.path.abspath(os.getcwd())+"/util/chromedriver")
         self.driver.get("https://github.com/")
         self.driver.maximize_window()
         self.driver.implicitly_wait(1)
         self.driver.find_element_by_xpath('/html/body/div[1]/header/div/div[2]/div[2]/a[1]').click()
-        self.driver.implicitly_wait(1)
-        #if username == '':
-            #username = input(' ' + ' Qual o nome de usuário do GitHub?')
+        self.driver.implicitly_wait(1)        
         self.driver.find_element_by_xpath('//*[@id="login_field"]').send_keys(defineusername())
-        #if pw == '':
-            #pw = input(' ' + ' Qual a senha do GitHub?')
         self.driver.find_element_by_xpath('//*[@id="password"]').send_keys(definepassword())
         self.driver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]').click()
         sleep(1)
 
 
     def _select_project(self):
-        #repo = ''#for test purposes define here.
-        #if repo == '':
-            #repo = input(' ' + ' Qual o nome do repositório?')
+        
         self.driver.find_element_by_xpath('//*[@title="' + definerepository() + '"]').click()     
 
     def _get_datetime(self):
         
         dates = self.driver.find_elements_by_tag_name('time-ago')
         datetime_commit = [dates[i].get_attribute('datetime') for i in range(len(dates))]
+        print(datetime_commit)
         return datetime_commit
     
-    def _datetime_converter(self):
+    def _datetime_converter(self, datetime_commit):
 
-        datetime_converted = datetime.strptime(datetime, '%d/%m/%y %H:%M:%S')
-        print(datetime_converted)
-        return datetime_converted
+        dates_list = [dt.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').date() for date in datetime_commit]
+        date_formatted = [dt.datetime.strptime(str(i), '%Y-%m-%d').strftime('%d-%m-%Y') for i in dates_list]
+        print(dates_list[2].day)
+        print(date_formatted)
+        return date_formatted
 
 
 
-#sleep(120)
 
-#my_bot = Secrets()
 my_bot = GitBot()
 my_bot._select_project()
-my_bot._get_datetime()
+datetime_commit = my_bot._get_datetime()
+my_bot._datetime_converter(datetime_commit)
 
